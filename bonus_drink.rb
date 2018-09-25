3ff5677750819a26ff774fbf61bd7fc889761e5b
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2018-09-25 10:57:07 +0900

    Finished answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,20 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    # Note: Someone might put "3.5" bottles that are impossible.
+    #   Therefore, it should be banned.
+    raise ArgumentError, "The argument 'amount' should be an integer." unless amount.is_a? Integer
+    # In addition to above, negative bottle number is impossible: Where's the "real" bottles?
+    #   Therefore, it should also be banned.
+    raise ArgumentError, "The argument 'amount' should be positive." if amount < 0
+
+    # And, let's keep it simple.
+    return 0 if amount < 1
+    addition = total_count_for(amount / 3)
+    addition += 1 if (addition + amount) % 3 == 0
+    return amount + addition
   end
+end
+
+if __FILE__ == $0
+  puts BonusDrink.total_count_for 100
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  describe "Positive integer as the argument" do
+    it "Should return proper-value" do
+      [
+        [0, 0],
+        [1, 1],
+        [3, 4],
+        [11, 16]
+      ].each {|v|
+        expect(BonusDrink.total_count_for(v[0])).to eq v[1]
+      }
+    end
+  end
+  describe "Error cases" do
+    it "Should raise an error in the case of non-integer type" do
+      expect { BonusDrink.total_count_for(Math::E) }.to raise_error(
+        ArgumentError, "The argument 'amount' should be an integer."
+      )
+      expect { BonusDrink.total_count_for("Doom!!") }.to raise_error(
+        ArgumentError, "The argument 'amount' should be an integer."
+      )
+    end
+    it "Should raise an error in the case of negative number" do
+      expect { BonusDrink.total_count_for(-1) }.to raise_error(
+        ArgumentError, "The argument 'amount' should be positive."
+      )
+    end
+  end
 end
